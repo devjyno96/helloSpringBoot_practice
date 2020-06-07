6807fe33a21622dcd63ff4ddb54cafc2d206ce22
stelladream <nykim@hansung.ac.kr>
2020-06-07 22:06:59 +0900

    CustomerController:CORS

--- src/main/java/kr/ac/hansung/cse/controller/CustomerController.java
+++ src/main/java/kr/ac/hansung/cse/controller/CustomerController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import kr.ac.hansung.cse.model.Customer;
 import kr.ac.hansung.cse.repo.CustomerRepository;
 
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
